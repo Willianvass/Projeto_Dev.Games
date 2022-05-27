b492f257a7f3db976ce3d7c816ff352dd5e267cc
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2022-05-26 22:23:35 -0300

    Add files via upload

---
+++ DevGames/DevGames/DevGames.API/Controllers/BoardsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevGames/DevGames/DevGames.API/Controllers/PostController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+    }
+}
